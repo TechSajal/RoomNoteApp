1ce80a1647a3dc4c0e4e1383892ce8874e689f7e
sajal <sajalkaushal3@gmail.com>
2022-08-24 15:41:16 +0530

    chore :- data package - userDao,repository,etc done

---
+++ app/src/androidTest/java/data/UserDataBase.kt
@@ -0,0 +1,27 @@
+package data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [User::class], version = 1, exportSchema = false)
+abstract class UserDataBase:RoomDatabase() {
+    abstract fun UserDao():UserDao
+    companion object{
+        @Volatile
+        private var INSTANCE:UserDataBase?=null
+        fun getDataBase(context: Context):UserDataBase{
+            val tempinstance = INSTANCE
+            if (tempinstance!=null){
+                return tempinstance
+            }
+            synchronized(this){
+                val instance = Room.databaseBuilder(context.applicationContext,UserDataBase::class.java,"user_database").build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+
+}
